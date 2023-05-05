15d3635e63ba7fb0463beef039afbd3698b0ab26
ManjushaFSD <manjusha.r.nair93@gmail.com>
2023-05-05 17:54:36 +0530

    Commit for deploy

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const logger = require('morgan')
+const path = require('path');
+
 require('./src/configs/db.config')
 const api = require("./src/routes/api")
 
@@ -12,6 +14,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.use('/api', api);
+
+app.use(express.static(path.join(__dirname, "./client/build")))
+app.get("*",(_,res)=>{
+res.sendFile(path.join(__dirname,"./client/build/index.html"),
+(err)=>{
+  res.status(500).send(err)
+}
+)
+
+})
 
 app.listen(3001, () => {
   console.log('Server running successfully on Port 3001')
---
+++ client/build/static/js/185.980ea96b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[185],{185:function(e,c,n){n.r(c),c.default={}}}]);
+//# sourceMappingURL=185.980ea96b.chunk.js.map
